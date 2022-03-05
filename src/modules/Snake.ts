c3ec3100f12553d7b4daeb3883668bdbe1a9b262
allenxz <13226271061@163.com>
2022-03-05 10:37:07 +0800

    update

--- src/modules/Snake.ts
+++ src/modules/Snake.ts
@@ -24,7 +24,20 @@ class Snake {
       throw new Error('蛇撞墙了')
     }
 
+    if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === val) {
+      if (val > this.X) {
+        val = this.X - 10
+      } else {
+        val = this.X + 10
+      }
+    }
+
+    this.moveBody()
+
     this.head.style.left = val + 'px'
+    
+    this.checkHeadBody()
+
   }
 
   set Y (val: number) {
@@ -33,12 +46,44 @@ class Snake {
     if (val < 0 || val > 290) {
       throw new Error('蛇撞墙了')
     }
+    
+    if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === val) {
+      if (val > this.Y) {
+        val = this.Y - 10
+      } else {
+        val = this.Y + 10
+      }
+    }
+
+    this.moveBody()
 
     this.head.style.top = val + 'px'
+
+    this.checkHeadBody()
+    
   }
 
   addBody () {
     this.element.insertAdjacentHTML('beforeend', '<div></div>')
+  }
+
+  moveBody () {
+    let i = this.bodies.length
+    while (--i > 0) {
+      const cur = this.bodies[i] as HTMLElement
+      const prev = this.bodies[i - 1] as HTMLElement
+      cur.style.left = prev.offsetLeft + 'px'
+      cur.style.top = prev.offsetTop + 'px'
+    }
+  }
+
+  checkHeadBody () {
+    for (let i = 1; i < this.bodies.length; i++) {
+      const bd = this.bodies[i] as HTMLElement
+      if (bd.offsetLeft === this.X && bd.offsetTop === this.Y) {
+        throw new Error('蛇撞到了身体！')
+      }
+    }
   }
 }
 
